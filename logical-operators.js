4d3b4a4b5202eddb26ed286b6fa0c101474fde15
divyanshsingh23 <dev.exists.here@gmail.com>
2025-05-20 19:19:45 +0530

    aage aur daal dunga

---
+++ logical-operators.js
@@ -0,0 +1,28 @@
+//logical operators
+
+let score = 10 ;
+let behavior = "good";
+
+let attendance = 90;
+// logical and 
+
+if ( score >= 9 && behavior == "good")   {
+    console.log("You are a good student  ");
+
+}
+
+
+//logical or 
+
+if ( score >= 9 || attendance >= 90)   {
+    console.log("You are eligible for scholarship  ");
+
+
+} 
+
+// //logical not
+
+if( score != 9)   {
+    console.log("You are not a good student  ");
+
+}
